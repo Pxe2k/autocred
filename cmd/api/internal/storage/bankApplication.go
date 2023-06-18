7bca2a4c60789c83c81bc94ce2f033c0f181ae5d
pxe2k <roariffic474@gmail.com>
2023-06-18 06:32:01 +0600

    application and template bank fields fixes

--- cmd/api/internal/storage/bankApplication.go
+++ cmd/api/internal/storage/bankApplication.go
@@ -4,17 +4,18 @@ import "gorm.io/gorm"
 
 type BankApplication struct {
 	gorm.Model
-	Bank            string `gorm:"size:100;" json:"bank"`
-	TrenchesNumber  int    `json:"trenchesNumber"`  // Кол-во траншей
-	CreditProductID uint   `json:"creditProductID"` // Кредитный продукт
-	KaskoID         *uint  `json:"kaskoID,omitempty"`
-	RoadHelpID      *uint  `json:"roadHelpID,omitempty"`
-	LifeInsuranceID *uint  `json:"lifeInsuranceID,omitempty"`
-	LoanAmount      int    `json:"loanAmount"` // Сумма займа
+	BankID          uint  `gorm:"size:100;" json:"bankID"`
+	TrenchesNumber  int   `json:"trenchesNumber"`            // Кол-во траншей
+	CreditProductID *uint `json:"creditProductID,omitempty"` // Кредитный продукт
+	KaskoID         *uint `json:"kaskoID,omitempty"`
+	RoadHelpID      *uint `json:"roadHelpID,omitempty"`
+	LifeInsuranceID *uint `json:"lifeInsuranceID,omitempty"`
+	LoanAmount      int   `json:"loanAmount"` // Сумма займа
 	ApplicationID   uint
 	CreditProduct   *BankProduct   `json:"creditProduct,omitempty"`
 	Kasko           *Kasko         `json:"kasko,omitempty"`
 	RoadHelp        *RoadHelp      `json:"roadHelp,omitempty"`
 	LifeInsurance   *LifeInsurance `json:"lifeInsurance,omitempty"`
+	Bank            Bank           `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"bank"`
 	BankResponse    BankResponse   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"bankResponse"`
 }
