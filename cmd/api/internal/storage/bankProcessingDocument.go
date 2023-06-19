b7f1c4c18e9b4f3ebb984c1c00436ef650f0c5c8
pxe2k <roariffic474@gmail.com>
2023-06-19 07:00:41 +0600

    blya zaebalsya))

---
+++ cmd/api/internal/storage/bankProcessingDocument.go
@@ -0,0 +1,50 @@
+package storage
+
+import "gorm.io/gorm"
+
+type BankProcessingDocument struct {
+	gorm.Model
+	Title              string `json:"title"`
+	Image              string `json:"image"`
+	File               string `json:"file"`
+	IndividualClientID uint   `json:"individualClientID"`
+}
+
+func (b *BankProcessingDocument) Save(db *gorm.DB) (*BankProcessingDocument, error) {
+	err := db.Debug().Create(&b).Error
+	if err != nil {
+		return &BankProcessingDocument{}, err
+	}
+	return b, nil
+}
+
+func (b *BankProcessingDocument) MultipleSave(db *gorm.DB, media []BankProcessingDocument) ([]BankProcessingDocument, error) {
+	err := db.Debug().Create(media).Error
+	if err != nil {
+		return []BankProcessingDocument{}, err
+	}
+	return media, nil
+}
+
+func (b *BankProcessingDocument) SoftDelete(db *gorm.DB, id uint) (int64, error) {
+	err := db.Debug().Model(&BankProcessingDocument{}).Where("id = ?", id).Take(&BankProcessingDocument{}).Delete(&BankProcessingDocument{})
+	if err != nil {
+		return 0, err.Error
+	}
+
+	if err.Error != nil {
+		return 0, err.Error
+	}
+
+	return err.RowsAffected, nil
+}
+
+func (b *BankProcessingDocument) All(db *gorm.DB, uid uint) ([]BankProcessingDocument, error) {
+	var media []BankProcessingDocument
+	err := db.Debug().Model(&BankProcessingDocument{}).Where("individual_client_id = ?", uid).Limit(100).Find(&media).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return media, nil
+}
